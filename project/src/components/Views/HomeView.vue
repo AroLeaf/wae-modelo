7a8315d7e7943ee5f169effc1324c22613e41cb4
RyuuRi <128174012+Yinkie66@users.noreply.github.com>
2025-06-05 21:07:44 +0200

    homeview

--- project/src/components/Views/HomeView.vue
+++ project/src/components/Views/HomeView.vue
@@ -1,11 +1,40 @@
 <script setup>
-import TheWelcome from '../TheWelcome.vue'
 import NavBar from '../NavBar.vue' 
+import MexicoMap from '../MexicoMap.vue'
+import Top10 from '../Top10.vue';
 </script>
 
 <template>
-  <NavBar />
+  <NavBar/>
   <main>
-    <TheWelcome />
+    <MexicoMap/>
+    <div class="dataContainer">
+      <h1>Top 10 temperatures</h1>
+      <top10/>
+    </div>
   </main>
 </template>
+
+<style>
+h1 {
+  background-color: rgb(17, 33, 66);
+  color: white;
+  padding: 5px;
+  border-radius: 20px;
+  text-align: center;
+  margin: 0;
+}
+
+div.dataContainer {
+  background-color: rgb(238, 238, 238);
+  border-radius: 10px;
+  padding: 1.5vh 3vw;
+  height: 45vh;
+}
+
+.data {
+  background-color: rgb(238, 238, 238);
+  color: rgb(17, 33, 66);
+  padding: 20px;
+}
+</style>
